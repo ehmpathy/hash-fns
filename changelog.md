6840d4c627553d448dbff20ccc3385d1ac29798f
Ulad Kasach <uladkasach@gmail.com>
2024-08-04 22:22:47 -0400

    feat(init): initialize based on procedure-fns

---
+++ changelog.md
@@ -0,0 +1,8 @@
+# Changelog
+
+## 1.0.0 (2024-07-28)
+
+
+### Features
+
+* **init:** initialize based on as-command ([fa91b7c](https://github.com/ehmpathy/procedure-fns/commit/fa91b7cd8b71aa62e4cb3d08282a8d14fff4f7c2))
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { Hash } from './domain/Hash';
+export { toHashSha256 } from './logic/casts/toHashSha256';
+export { toHashSha256Sync } from './logic/casts/toHashSha256Sync';
+export { isHashSha256 } from './logic/checks/isHashSha256';
